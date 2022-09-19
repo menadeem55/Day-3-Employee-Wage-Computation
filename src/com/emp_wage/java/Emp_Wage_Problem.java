aed0a202ee68f21fabd8aac5e76e785aee8fb1eb
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-19 17:44:43 +0530

    [Nadeem] Welcome to the Employee Wage Problem Programming

--- src/com/emp_wage/java/Emp_Wage_Problem.java
+++ src/com/emp_wage/java/Emp_Wage_Problem.java
@@ -1,9 +1,21 @@
 package com.emp_wage.java;
 
+import java.util.Random;
+
 public class Emp_Wage_Problem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+
+Random random = new Random();
+		
+		int randomNum = random.nextInt(2);
+		
+		if(randomNum == 1)
+			System.out.println("Employee  is Present");
+		else
+			System.out.println("Employee  is Absent");
+		
 
 	}
 
