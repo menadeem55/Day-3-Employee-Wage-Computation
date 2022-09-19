1cf136346c4df30aef290821e06d940d31c4eab5
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-19 19:21:38 +0530

    [Nadeem] Employee Salary programming using switch case logic

--- src/com/emp_wage/java/Emp_Wage_Problem.java
+++ src/com/emp_wage/java/Emp_Wage_Problem.java
@@ -4,34 +4,53 @@ import java.util.Random;
 
 public class Emp_Wage_Problem {
 
-		public static void main(String[] args) {
-			System.out.println("Welcome to Employee Wage Computation Program");
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
 
-				Random random = new Random();
-				
-				
-				int wagePerHrs = 20;
-				int fullDayHrs = 8;
-				int halfDaysHrs = 4;
-				int salary = 0;
-		
-				int randomNum = random.nextInt(3);
-		
-				if(randomNum == 1)
-				{	
-					System.out.println("Employee  is Present");
-						salary = wagePerHrs * fullDayHrs;
-				}
-				else if(randomNum == 2)
-				{
-					salary = wagePerHrs * halfDaysHrs;
-						System.out.println("Employee is Present for Half Day");
-				}
-				else
-					
-					System.out.println("Employee  is Absent");
-		
-				System.out.println("Salary is: "+salary);
+		Random random = new Random();
+
+		int wagePerHrs = 20;
+		int fullDayHrs = 8;
+		int halfDaysHrs = 4;
+		int salary = 0;
+
+		int randomNum = random.nextInt(3);
+
+		switch (randomNum) {
+		case 0:
+			
+			System.out.println("Employee is Absent");
+			break;
+
+		case 1:
+			
+			System.out.println("Employee is Present");
+				salary = wagePerHrs * fullDayHrs;
+			break;
+
+		case 2:
+			System.out.println("Employee is Present for Half Day");
+				salary = wagePerHrs * halfDaysHrs;
+				break;
+		}
+
+		System.out.println("Salary is: " + salary);
+
+//				if(randomNum == 1)
+//				{	
+//					System.out.println("Employee  is Present");
+//						salary = wagePerHrs * fullDayHrs;
+//				}
+//				else if(randomNum == 2)
+//				{
+//					salary = wagePerHrs * halfDaysHrs;
+//						System.out.println("Employee is Present for Half Day");
+//				}
+//				else
+//					
+//					System.out.println("Employee  is Absent");
+//		
+//				System.out.println("Salary is: "+salary);
 
 	}
 
