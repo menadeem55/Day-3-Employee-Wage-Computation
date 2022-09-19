593c6800d9027636eb659faaf2132362dae7744a
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-19 17:35:56 +0530

    [Nadeem] Welcome to the Employee Wage Problem Programming

---
+++ src/com/emp_wage/java/Emp_Wage_Problem.java
@@ -0,0 +1,10 @@
+package com.emp_wage.java;
+
+public class Emp_Wage_Problem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
