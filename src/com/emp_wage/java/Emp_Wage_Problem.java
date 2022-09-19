f1737a86a30bb6199ac2fb608a6faf4609d8c84b
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-19 18:01:45 +0530

    [Nadeem] Employee Full Time Programming

--- src/com/emp_wage/java/Emp_Wage_Problem.java
+++ src/com/emp_wage/java/Emp_Wage_Problem.java
@@ -4,18 +4,27 @@ import java.util.Random;
 
 public class Emp_Wage_Problem {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+		public static void main(String[] args) {
+			System.out.println("Welcome to Employee Wage Computation Program");
 
-Random random = new Random();
+				Random random = new Random();
+				
+				
+				int wagePerHrs = 20;
+				int fullDayHrs = 8;
+				int salary = 0;
 		
-		int randomNum = random.nextInt(2);
+				int randomNum = random.nextInt(2);
 		
-		if(randomNum == 1)
-			System.out.println("Employee  is Present");
-		else
-			System.out.println("Employee  is Absent");
+				if(randomNum == 1)
+				{	
+					System.out.println("Employee  is Present");
+						salary = wagePerHrs * fullDayHrs;
+				}
+				else
+					System.out.println("Employee  is Absent");
 		
+				System.out.println("Salary is:"+salary);
 
 	}
 
