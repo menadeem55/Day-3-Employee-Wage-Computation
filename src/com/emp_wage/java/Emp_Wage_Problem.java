f4ff85021b84f04568ae682d55c2dce425bf5ecf
me.nadeem55@gmail.com <me.nadeem55@gmail.com>
2022-09-19 20:29:16 +0530

    [Nadeem] Employee Salary programming based on condition logic

--- src/com/emp_wage/java/Emp_Wage_Problem.java
+++ src/com/emp_wage/java/Emp_Wage_Problem.java
@@ -11,42 +11,47 @@ public class Emp_Wage_Problem {
 		Random random = new Random();
 
 		int wagePerHrs = 20;
-		int fullDayHrs = 8;
+		int fullDayHrs = 100;
 		int halfDaysHrs = 4;
 		int salary = 0;
 		int monthlySalary = 0;
 		int workingDaysPerMonth = 20;
+		int days = 0;
+		int monthlyHrs = 0;
 
-		for (int i = 1; i <= workingDaysPerMonth; i++) {
-
+		
+		while (days != 20 && monthlyHrs != 100) {
+				days++;
+				
+				
 			int randomNum = random.nextInt(3);
 
 			switch (randomNum) {
 			case 0:
 
-				System.out.println("Employee is Absent");
+			//	System.out.println("Employee is Absent");
 				break;
 
 			case 1:
 
-				System.out.println("Employee is Present");
+			//	System.out.println("Employee is Present");
 				salary = wagePerHrs * fullDayHrs;
+				monthlyHrs = monthlyHrs + fullDayHrs;
 				break;
 
 			case 2:
-				System.out.println("Employee is Present for Half Day");
+			//	System.out.println("Employee is Present for Half Day");
 				salary = wagePerHrs * halfDaysHrs;
-				
+				monthlyHrs = monthlyHrs + halfDaysHrs;
 
 			}
 
-			
 			monthlySalary = monthlySalary + salary;
-			System.out.println("Salary is: " + salary);
 			
+			System.out.println("Day " + days + " Salary is: " + salary);
+
 		}
-			System.out.println("monthly salary: "+ monthlySalary);
-			
+		System.out.println("monthly salary: " + monthlySalary);
 
 //				if(randomNum == 1)
 //				{	
